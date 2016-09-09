822db42e3f4a856244744bb0476edb27b26e6874
PeterBourgonje <bourgonje@gmail.com>
2016-09-09 16:27:11 +0200

    - adding the opennlp sentiment analyzer which can be trained with annotated data. Just for some friday afternoon fun.

--- src/main/java/de/dkt/eservices/esentimentanalysis/modules/CoreNLPSentimentAnalyzer.java
+++ src/main/java/de/dkt/eservices/esentimentanalysis/modules/CoreNLPSentimentAnalyzer.java
@@ -37,22 +37,35 @@ public class CoreNLPSentimentAnalyzer {
 	
 	public static void main(String[] args) throws Exception {
 
-		String docFolder = "C:\\Users\\pebo01\\Desktop\\data\\FRONTEO\\complaintsIndividualFiles";
-		File df = new File(docFolder);
-		for (File f : df.listFiles()){
-			String fileContent = readFile(f.getAbsolutePath(), StandardCharsets.UTF_8);
-			double sentVal = getSentiment(fileContent);
-			System.out.println("Filename:" + f.getAbsolutePath());
-			System.out.println("sentiment value:" + sentVal);
-			System.out.println("\n");
-		}
+//		String docFolder = "C:\\Users\\pebo01\\Desktop\\data\\FRONTEO\\complaintsIndividualFiles";
+//		File df = new File(docFolder);
+//		for (File f : df.listFiles()){
+//			String fileContent = readFile(f.getAbsolutePath(), StandardCharsets.UTF_8);
+//			double sentVal = getSentiment(fileContent);
+//			System.out.println("Filename:" + f.getAbsolutePath());
+//			System.out.println("sentiment value:" + sentVal);
+//			System.out.println("\n");
+//		}
 		
 
-		//getSentiment("love great wonderful awesome cool nice good");
-		//getSentiment("shit fuck damn piss off screw bad angry");
+		System.out.println(getSentiment("love great wonderful awesome cool nice good"));
+		System.out.println(getSentiment("shit fuck damn piss off screw bad angry"));
 		//String str = "1297685	24-03-2015	Debt collection	Cont'd attempts collect debt not owed	Debt is not mine	\"I find this medical debt reported on my credit report  but I do not remember ever owing a bill with a remaining balance of {$7.00}. It looks like the company dated the opening of this debt XX/XX/2010. I looked for the collection company on line  but it is as though it does n't exist. It appears to have impact on my credit score. I am willing to pay it if I could get an itemized bill showing the date of the procedure and when and what my medical insurance company reports about it. Also  if the company no longer exist  IT WILL NEED TO BE REMOVE FROM MY CREDIT REPORT. My complaint is that this company has not sent me a bill showing the details of my so called debt  but has reported to the credit bureau that I owe this debt. This is harming my credit score and it is not making it 's contact information available to me. How am I to clear up my credit report if this company does n't exist  but they have reported that I owe a debt that I have no explanation for? _\" Company chooses not to provide a public response \"Healthcare Collections-I";
 		//int sentVal = getSentiment(str);
 		//System.out.println("Sentiment value:" + sentVal);
+		
+		
+		//TODO: keep this for somewhere, as it seems to be a neat way of changing the language/annotators in corenlp to german/anything else we included in the pom file
+//		Properties germanProps = new Properties();
+//		germanProps.setProperty("annotators", "tokenize, ssplit, parse");
+//		germanProps.put("tokenize.language", "de");
+//		//germanProps.put("parse.model", "edu/stanford/nlp/models/lexparser/germanFactored.ser.gz");
+//		germanProps.put("parse.model", "edu/stanford/nlp/models/parser/nndep/UD_German.gz");
+		
+		
+		
+		
+		
 
 	}
 	
