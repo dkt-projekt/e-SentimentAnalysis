d2e8f9d2a14c40f99bf241a210dc845ef88be922
PeterBourgonje <bourgonje@gmail.com>
2017-06-09 14:56:34 +0200

    - non-working (backup) commit of corenlp training methods

--- src/main/java/de/dkt/eservices/esentimentanalysis/modules/CoreNLPSentimentAnalyzer.java
+++ src/main/java/de/dkt/eservices/esentimentanalysis/modules/CoreNLPSentimentAnalyzer.java
@@ -3,12 +3,15 @@ package de.dkt.eservices.esentimentanalysis.modules;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.io.FilenameUtils;
@@ -16,15 +19,35 @@ import org.omg.Messaging.SyncScopeHelper;
 
 import com.hp.hpl.jena.rdf.model.Model;
 
+import de.dkt.common.filemanagement.FileFactory;
 import de.dkt.common.niftools.NIFReader;
 import de.dkt.common.niftools.NIFWriter;
+import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.Label;
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations.GoldClass;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+import edu.stanford.nlp.parser.lexparser.TreeBinarizer;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.sentiment.BuildBinarizedDataset;
+import edu.stanford.nlp.sentiment.CollapseUnaryTransformer;
+import edu.stanford.nlp.sentiment.RNNOptions;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations.SentimentAnnotatedTree;
+import edu.stanford.nlp.sentiment.SentimentCostAndGradient;
+import edu.stanford.nlp.sentiment.SentimentModel;
+import edu.stanford.nlp.sentiment.SentimentTraining;
+import edu.stanford.nlp.sentiment.SentimentUtils;
 import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.Trees;
 import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.Generics;
+import edu.stanford.nlp.util.Pair;
+import edu.stanford.nlp.util.StringUtils;
 import opennlp.tools.sentdetect.SentenceDetector;
 import opennlp.tools.util.Span;
 
@@ -40,8 +63,249 @@ public class CoreNLPSentimentAnalyzer {
 			  return new String(encoded, encoding);
 			}
 	
+	 public static void extractLabels(Map<Pair<Integer, Integer>, String> spanToLabels, List<HasWord> tokens, String line) {
+		String[] pieces = line.trim().split("\\s+");
+		if (pieces.length == 0) {
+			return;
+		}
+		if (pieces.length == 1) {
+			String error = "Found line with label " + line + " but no tokens to associate with that line";
+			throw new RuntimeException(error);
+		}
+		for (int i = 0; i < tokens.size() - pieces.length + 2; ++i) {
+			boolean found = true;
+			for (int j = 1; j < pieces.length; ++j) {
+				if (!tokens.get(i + j - 1).word().equals(pieces[j])) {
+					found = false;
+					break;
+				}
+			}
+			if (found) {
+				spanToLabels.put(new Pair<>(i, i + pieces.length - 1), pieces[0]);
+			}
+		}
+	}
+
+	public static void setPredictedLabels(Tree tree) {
+		if (tree.isLeaf()) {
+			return;
+		}
+	}
+
+	public static void setUnknownLabels(Tree tree, Integer defaultLabel) {
+		if (tree.isLeaf()) {
+			return;
+		}
+	}
+
+	public static boolean setSpanLabel(Tree tree, Pair<Integer, Integer> span, String value) {
+		if (!(tree.label() instanceof CoreLabel)) {
+			throw new AssertionError("Expected CoreLabels");
+		}
+		CoreLabel label = (CoreLabel) tree.label();
+		if (label.get(CoreAnnotations.BeginIndexAnnotation.class).equals(span.first)
+				&& label.get(CoreAnnotations.EndIndexAnnotation.class).equals(span.second)) {
+			label.setValue(value);
+			return true;
+		}
+		if (label.get(CoreAnnotations.BeginIndexAnnotation.class) > span.first
+				&& label.get(CoreAnnotations.EndIndexAnnotation.class) < span.second) {
+			return false;
+		}
+		for (Tree child : tree.children()) {
+			if (setSpanLabel(child, span, value)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// https://github.com/stanfordnlp/CoreNLP/blob/master/src/edu/stanford/nlp/sentiment/BuildBinarizedDataset.java
+	public String prepareTrainingData(String inputPath){
+		// code below is copied/stripped from stanford github (check link above
+		// for more details/comments/explanation
+		
+		List<Tree> trainingTrees = new ArrayList<Tree>();
+		
+		CollapseUnaryTransformer transformer = new CollapseUnaryTransformer();
+		String parserModel = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"; // TODO: make argument (allow/test german)
+		SentimentModel sentimentModel = null;
+		LexicalizedParser parser = LexicalizedParser.loadModel(parserModel);
+		TreeBinarizer binarizer = TreeBinarizer.simpleTreeBinarizer(parser.getTLPParams().headFinder(),	parser.treebankLanguagePack());
+		String tmpFilePath = null;
+		PrintWriter tempFile = null;
+		try {
+			tmpFilePath = FileFactory.generateOrCreateFileInstance("sentimentModels" + File.separator + "corenlpTraining.tmp").getAbsolutePath(); // would rather not do this with temp files and return List<Tree> directly here, but that was problematic later on with reading the gold labels...
+			tempFile = new PrintWriter(tmpFilePath);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+
+		String text = IOUtils.slurpFileNoExceptions(inputPath);
+		String[] chunks = text.split("\\n\\s*\\n+"); // need blank line to
+		for (String chunk : chunks) {
+			if (chunk.trim().isEmpty()) {
+				continue;
+			}
+			String[] lines = chunk.trim().split("\\n");
+			String sentence = lines[0];
+			StringReader sin = new StringReader(sentence);
+			DocumentPreprocessor document = new DocumentPreprocessor(sin);
+			document.setSentenceFinalPuncWords(new String[] { "\n" });
+			List<HasWord> tokens = document.iterator().next();
+			Integer mainLabel = new Integer(tokens.get(0).word());
+			tokens = tokens.subList(1, tokens.size());
+			Map<Pair<Integer, Integer>, String> spanToLabels = Generics.newHashMap();
+			for (int i = 1; i < lines.length; ++i) {
+				extractLabels(spanToLabels, tokens, lines[i]);
+			}
+			Tree tree = parser.apply(tokens);
+			Tree binarized = binarizer.transformTree(tree);
+			Tree collapsedUnary = transformer.transformTree(binarized);
+			if (sentimentModel != null) {
+				Trees.convertToCoreLabels(collapsedUnary);
+				SentimentCostAndGradient scorer = new SentimentCostAndGradient(sentimentModel, null);
+				scorer.forwardPropagateTree(collapsedUnary);
+				setPredictedLabels(collapsedUnary);
+			} else {
+				setUnknownLabels(collapsedUnary, mainLabel);
+			}
+			Trees.convertToCoreLabels(collapsedUnary);
+			collapsedUnary.indexSpans();
+			for (Map.Entry<Pair<Integer, Integer>, String> pairStringEntry : spanToLabels.entrySet()) {
+				setSpanLabel(collapsedUnary, pairStringEntry.getKey(), pairStringEntry.getValue());
+			}
+			
+			//trainingTrees.add(collapsedUnary);
+			//System.out.println("Debugging collaped Unary:" + collapsedUnary);
+			tempFile.println(collapsedUnary);
+		}
+		tempFile.close();
+		
+		
+		return tmpFilePath;
+		
+	}
+	
+	public void trainModel(String trainingTreesFile, String modelName){
+		
+		SentimentTraining st = new SentimentTraining();
+		String devPath = "dummy"; //TODO: check what this is supposed to contain (the same trees as the training ones? Some tuning set?)
+
+		RNNOptions op = new RNNOptions();
+		boolean runGradientCheck = false;
+		boolean runTraining = true; //  TODO: check why the hell you would want to have this to false in a trainModel method? Is there a reason for this?
+		boolean filterUnknown = false;
+
+		String modelPath = null;
+		try {
+			//modelPath = FileFactory.generateFileInstance("sentimentModels" + File.separator + modelName + ".bin").getAbsolutePath();
+			modelPath = FileFactory.generateOrCreateFileInstance("sentimentModels" + File.separator + modelName).getAbsolutePath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//TODO: check if I want any of this below...
+//		for (int argIndex = 0; argIndex < args.length;) {
+//			if (args[argIndex].equalsIgnoreCase("-train")) {
+//				runTraining = true;
+//				argIndex++;
+//			} else if (args[argIndex].equalsIgnoreCase("-gradientcheck")) {
+//				runGradientCheck = true;
+//				argIndex++;
+//			} else if (args[argIndex].equalsIgnoreCase("-trainpath")) {
+//				trainPath = args[argIndex + 1];
+//				argIndex += 2;
+//			} else if (args[argIndex].equalsIgnoreCase("-devpath")) {
+//				devPath = args[argIndex + 1];
+//				argIndex += 2;
+//			} else if (args[argIndex].equalsIgnoreCase("-model")) {
+//				modelPath = args[argIndex + 1];
+//				argIndex += 2;
+//			} else if (args[argIndex].equalsIgnoreCase("-filterUnknown")) {
+//				filterUnknown = true;
+//				argIndex++;
+//			} else {
+//				int newArgIndex = op.setOption(args, argIndex);
+//				if (newArgIndex == argIndex) {
+//					throw new IllegalArgumentException("Unknown argument " + args[argIndex]);
+//				}
+//				argIndex = newArgIndex;
+//			}
+//		}
+
+//		if (filterUnknown) {
+//			trainingTrees = SentimentUtils.filterUnknownRoots(trainingTrees);
+//		}
+
+//		List<Tree> devTrees = null;
+//		if (devPath != null) {
+//			devTrees = SentimentUtils.readTreesWithGoldLabels(devPath);
+//			if (filterUnknown) {
+//				devTrees = SentimentUtils.filterUnknownRoots(devTrees);
+//			}
+//		}
+
+		List<Tree> trainingTrees = SentimentUtils.readTreesWithGoldLabels(trainingTreesFile);
+		SentimentModel model = new SentimentModel(op, trainingTrees);
+
+//		if (op.trainOptions.initialMatrixLogPath != null) {
+//			StringUtils.printToFile(new File(op.trainOptions.initialMatrixLogPath), model.toString(), false, false,
+//					"utf-8");
+//		}
+
+		// TODO: need to handle unk rules somehow... at test time the tree
+		// structures might have something that we never saw at training
+		// time. for example, we could put a threshold on all of the
+		// rules at training time and anything that doesn't meet that
+		// threshold goes into the unk. perhaps we could also use some
+		// component of the accepted training rules to build up the "unk"
+		// parameter in case there are no rules that don't meet the
+		// threshold
+
+//		if (runGradientCheck) {
+//			st.runGradientCheck(model, trainingTrees);
+//		}
+
+		List<Tree> devTrees = null;
+//		if (devPath != null) {
+//			devTrees = SentimentUtils.readTreesWithGoldLabels(devPath);
+//			if (filterUnknown) {
+//				devTrees = SentimentUtils.filterUnknownRoots(devTrees);
+//			}
+//		}
+		//TODO: check what the devTrees are supposed to contain? tuning data? we don't have any, so:
+		devTrees = trainingTrees; 
+		 	    Label label = trainingTrees.get(0).label();
+			    if (!(label instanceof CoreLabel)) {
+			      throw new IllegalArgumentException("CoreLabels required to get the attached gold class");
+			    }
+			    System.out.println("DEBUGGING gold class:" + ((CoreLabel) label).get(GoldClass.class));
+		
+		if (runTraining) {
+//			System.out.println("DEBUGGING MODEL:" + model);
+//			System.out.println("DEBUGGING MODELPATH:" + modelPath);
+//			System.out.println("DEBUGGING trainingTrees:" + trainingTrees);
+//			System.out.println("DEBUGGING trainindevTrees:" + devTrees);
+			st.train(model, modelPath, trainingTrees, devTrees);
+			System.out.println("DEBUG NUM CLASSES:" + model.numClasses);
+			model.saveSerialized(modelPath);
+		}
+	}
+
+		
+	
+	
+	
 	public static void main(String[] args) throws Exception {
 
+		CoreNLPSentimentAnalyzer sa = new CoreNLPSentimentAnalyzer();
+		String fp = sa.prepareTrainingData("C:\\Users\\pebo01\\Desktop\\corenlpsentimenttrainingsample.txt");
+		sa.trainModel(fp, "corenlpTrainingDummy");
+		// TODO: the BuildBinarizedDataset output is in the wrong format. Figure out how to fix this (see also https://stackoverflow.com/questions/44458405/corenlp-sentiment-training-data-in-wrong-format)
+		
 //		
 //		System.out.println(getSentiment("This is good. It is very great. Better."));
 //		System.exit(1);
@@ -73,35 +337,35 @@ public class CoreNLPSentimentAnalyzer {
 //		//germanProps.put("parse.model", "edu/stanford/nlp/models/lexparser/germanFactored.ser.gz");
 //		germanProps.put("parse.model", "edu/stanford/nlp/models/parser/nndep/UD_German.gz");
 		
-		PrintWriter out = new PrintWriter(new File("C:\\Users\\pebo01\\Desktop\\debug.txt"));
-		
-		Date d1 = new Date();
-		
-		try {
-			String fileContent = readFile("C:\\Users\\pebo01\\Desktop\\data\\sentimentData\\sanders-twitter-0.2\\stuffDownloadedFromGithub\\testData.txt", StandardCharsets.UTF_8);
-			String[] lines = fileContent.split("\\n");
-			for (String l : lines){
-				String v = null;
-				Double polarity = getSentiment(l);
-				
-				if (polarity > 1){
-					v = "1";
-				}
-				else {
-					v = "0";
-				}
-				System.out.println(v + "\t" + l);
-				out.write(v + "\t" + l + "\n");
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		out.close();
-		Date d2 = new Date();
-		System.out.println("Start time:" + d1);
-		System.out.println("End time:" + d2);
+//		PrintWriter out = new PrintWriter(new File("C:\\Users\\pebo01\\Desktop\\debug.txt"));
+//		
+//		Date d1 = new Date();
+//		
+//		try {
+//			String fileContent = readFile("C:\\Users\\pebo01\\Desktop\\data\\sentimentData\\sanders-twitter-0.2\\stuffDownloadedFromGithub\\testData.txt", StandardCharsets.UTF_8);
+//			String[] lines = fileContent.split("\\n");
+//			for (String l : lines){
+//				String v = null;
+//				Double polarity = getSentiment(l);
+//				
+//				if (polarity > 1){
+//					v = "1";
+//				}
+//				else {
+//					v = "0";
+//				}
+//				System.out.println(v + "\t" + l);
+//				out.write(v + "\t" + l + "\n");
+//			}
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		out.close();
+//		Date d2 = new Date();
+//		System.out.println("Start time:" + d1);
+//		System.out.println("End time:" + d2);
 
 	}
 	
